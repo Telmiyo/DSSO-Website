e7154c7872b05479e5016d3633477444a8f1b38f
Telmo Beroiz <telmiyodev@gmail.com>
2022-06-01 00:53:29 +0200

    Team

---
+++ pages/team.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function Team() {
+  return (
+    <div>
+        Hello Team!
+        {/*Member Frame*/}
+        <div className='bg-red-200 w-20 h-20'></div>
+    </div>
+  )
+}
