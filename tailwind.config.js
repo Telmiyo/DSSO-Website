261397943e32df7165439b4f3ec6af5e5d185c5c
Omicrxn <alexavila0128@gmail.com>
2022-05-31 17:03:38 +0200

    minor things

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
+  mode: 'jit',
+  content:[
+  ],
   content: [
+    './public/**/*.html',
+    // './src/**/*.{js,jsx,ts,tsx,vue}',
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
