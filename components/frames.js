a5ee78ac835e3ad4628cf75e02410c2d2d36cccb
Telmo Beroiz <telmiyodev@gmail.com>
2022-06-01 03:47:40 +0200

    Member Filters Implemented

--- components/frames.js
+++ components/frames.js
@@ -27,16 +27,16 @@ const SetRoleColor = (_role) => {
 
 export const MemberFrame = ({ src, name, role, github, linkdn }) => {
     return (
-        <div className='rounded-xl shadow-xl w-1/2'>
-            <div className='flex flex-col justify-around h-100 space-y-2 py-4 items-center'>
+        <div className='rounded-xl shadow-xl w-[216px] min-h-[285px]'>
+            <div className='flex flex-col justify-around h-100 space-y-4 py-4 items-center'>
 
                 <Image src={src} width={120} height={120} objectFit="cover" className="rounded-full" />
 
                 <p>{name}</p>
-                <div className={`${SetRoleColor(role)} text-center rounded-xl w-40`}>{role}</div>
+                <div className={`${SetRoleColor(role)} text-center text-white rounded-xl w-40`}>{role}</div>
                 <div className="flex flex-row space-x-4">
                     <a href={github}>
-                        <IoLogoGithub size={35}/>
+                        <IoLogoGithub size={35} />
                     </a>
                     <a href={linkdn}>
                         <IoLogoLinkedin size={35} className="text-blue-500 " />
@@ -52,12 +52,12 @@ export const ToolFrame = ({ src, role, name, desc }) => {
         <div className='rounded-xl shadow-xl w-[468px]'>
             <div className='flex flex-row justify-start h-100 space-x-4 p-4 items-center'>
                 <div className={`${SetRoleColor(role)} text-center rounded-xl p-1`}>
-                <Image src={src} width={50} height={50} objectFit="cover" className="rounded-full" />
+                    <Image src={src} width={50} height={50} objectFit="cover" className="rounded-full" />
                 </div>
                 <p>{name}</p>
-                
+
             </div>
-                <p className='px-4 pb-4'>{desc}</p>
+            <p className='px-4 pb-4'>{desc}</p>
         </div>
     )
 }
--- pages/team.js
+++ pages/team.js
@@ -3,29 +3,37 @@ import NextLink from "next/link";
 import { getAllFilesMetadata } from "../utils/mdx";
 import { MemberFrame, ToolFrame } from "../components/frames";
 
+
 export default function Team({ teammembers }) {
-  return (
-    <div>
-      <div className="grid grid-cols-3 gap-6">
-        {teammembers.map((teammember) => (
-          <div>
-            <NextLink key={teammember.slug} href={`/${teammember.slug}`}>
-              {/*Member Frame*/}
-              <div className="bg-red-200 w-20 h-20"></div>
-            </NextLink>
-          </div>
-        ))}
-      </div>
-      <MemberFrame src="/team/David_montufo.jpg" name="Telmo Beroiz Bilbao" role="Putita"/>
-      <ToolFrame src="/contents/tools/drive.png" name="Google Drive" role="Artist" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo. consectetur adipiscing elit."></ToolFrame>
-    </div>
-  );
+    const actualRole = "Programmer"
+    return (
+        <div>
+            <div className="">
+                <div className="grid grid-cols-2 md:grid-cols-4 lg:px-40 2xl:px-80 gap-4 justify-items-center">
+                    {teammembers.filter(teammember => teammember.role.includes(actualRole)).map(filteredmember => (
+                        <MemberFrame src={`/team/${filteredmember.slug}.png`} name={filteredmember.name} role={filteredmember.role} />
+                    ))}
+                    {/* 
+                    {teammembers.map((teammember) => (
+                        
+                        <MemberFrame src={`/team/${teammember.slug}.png`} name={teammember.name} role={teammember.role} />
+                        // <NextLink key={teammember.slug} href={`/${teammember.slug}`}>
+
+                        // </NextLink>
+
+                    ))} */}
+                </div>
+            </div>
+
+            <ToolFrame src="/contents/tools/drive.png" name="Google Drive" role="Artist" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo. consectetur adipiscing elit."></ToolFrame>
+        </div>
+    );
 }
 
 export async function getStaticProps() {
-  const teammembers = await getAllFilesMetadata();
-  console.log(teammembers);
-  return {
-    props: { teammembers },
-  };
+    const teammembers = await getAllFilesMetadata();
+    console.log(teammembers);
+    return {
+        props: { teammembers },
+    };
 }
