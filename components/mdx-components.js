d72d0af655a3f8098c12bc5971b8fe0c3e471135
Oscar Cuatrecasas <oscar.cuatrecasas@est.fib.upc.edu>
2022-06-11 13:49:27 +0200

    push

--- components/mdx-components.js
+++ components/mdx-components.js
@@ -1,5 +1,6 @@
 import { Tabs } from "flowbite-react";
 import Youtube from "react-youtube";
+import Vimeo from "react-vimeo";
 import JobCard from "./job-card";
 import Lottie from "react-lottie";
 import Image from 'next/image'
@@ -8,6 +9,7 @@ import SketchfabCard from './sketchfab'
 export default {
    Tabs,
    Youtube,
+   Vimeo,
    JobCard,
    Lottie,
    Image,
