f334714a2acc08f9b72fb9fb6b1d318f9380fa03
Telmo Beroiz <telmiyodev@gmail.com>
2022-06-13 01:45:53 +0200

    HydrationFixed

--- components/card.js
+++ components/card.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Card = (props) => {
   return (
-    <div className="  w-full h-full contrast-50 hover:opacity-100 hover:brightness-100  hover:grayscale-0 transition-all ease-in delay-50 duration-200 hover:scale-[1.025] hover:shadow-xl">
+    <a href={props.path} target="_blank" className="w-full h-full contrast-50 hover:opacity-100 hover:brightness-100  hover:grayscale-0 transition-all ease-in delay-50 duration-200 hover:scale-[1.025] hover:shadow-xl">
       <Image
         src={props.path}
         alt={props.title}
@@ -12,7 +12,7 @@ const Card = (props) => {
         layout="responsive"
         className="object-cover w-full h-full invert hover:invert-0 transition-all ease-in delay-100 duration-200"
       />
-    </div>
+    </a>
   );
 };
 
--- pages/gallery.js
+++ pages/gallery.js
@@ -4,14 +4,9 @@ import Card from "../components/card";
 import PageLayout from "../layouts/page-layout";
 export default function Gallery() {
   return (
-    <PageLayout className={"bg-gray-200 blur-10"}>
+    <PageLayout>
       <div className=" md:mx-36 md:my-5 grid grid-cols-1 gap-y-6 md:grid-cols-3 md:grid-rows-4 md:gap-6">
-        <a
-          className="md:row-span-2 md:col-span-1 "
-          id="modal-test"
-          href="/contents/gallery/1.png"
-          target="_blank"
-        >
+        <div className="md:row-span-2 md:col-span-1 ">
           <Card
             title="Influence"
             subtitle="Design and Development"
@@ -19,30 +14,20 @@ export default function Gallery() {
             width="300"
             height="600"
           />
-        </a>
+        </div>
 
-        <a
-          className=" md:row-span-1 md:col-span-2"
-          href="/contents/gallery/2.png"
-          target="_blank"
-        >
-          <Link href="/contents/gallery/2.png" target>
-            <a>
-              <Card
-                title="Insomnia"
-                subtitle="Design and Development"
-                path="/contents/gallery/2.png"
-                width="600"
-                height="300"
-              />
-            </a>
-          </Link>
-        </a>
-        <a
-          className="md:row-span-1 md:col-span-1"
-          href="/contents/gallery/3.png"
-          target="_blank"
-        >
+        <div className=" md:row-span-1 md:col-span-2">
+          <div>
+            <Card
+              title="Insomnia"
+              subtitle="Design and Development"
+              path="/contents/gallery/2.png"
+              width="600"
+              height="300"
+            />
+          </div>
+        </div>
+        <div className="md:row-span-1 md:col-span-1">
           <Card
             title="Influence"
             subtitle="Design and Development"
@@ -50,12 +35,8 @@ export default function Gallery() {
             width="300"
             height="300"
           />
-        </a>
-        <a
-          className="md:row-span-2 md:col-span-1"
-          href="/contents/gallery/4.png"
-          target="_blank"
-        >
+        </div>
+        <div className="md:row-span-2 md:col-span-1">
           <Card
             title="Influence"
             subtitle="Design and Development"
@@ -63,12 +44,8 @@ export default function Gallery() {
             width="300"
             height="600"
           />
-        </a>
-        <a
-          className="md:row-span-2 md:col-span-2"
-          href="/contents/gallery/5.png"
-          target="_blank"
-        >
+        </div>
+        <div className="md:row-span-2 md:col-span-2">
           <Card
             title="Influence"
             subtitle="Design and Development"
@@ -76,12 +53,8 @@ export default function Gallery() {
             width="600"
             height="600"
           />
-        </a>
-        <a
-          className="md:row-span-1 md:col-span-1"
-          href="/contents/gallery/6.png"
-          target="_blank"
-        >
+        </div>
+        <div className="md:row-span-1 md:col-span-1">
           <Card
             title="Influence"
             subtitle="Design and Development"
@@ -89,7 +62,7 @@ export default function Gallery() {
             width="300"
             height="300"
           />
-        </a>
+        </div>
       </div>
     </PageLayout>
   );
