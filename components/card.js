c266c6278b102cf7dcc297adf5a1269925f00b50
Telmo Beroiz <telmiyodev@gmail.com>
2022-06-13 01:23:16 +0200

    Gallery Image Added

--- components/card.js
+++ components/card.js
@@ -1,16 +1,17 @@
 import React from "react";
-import Image from 'next/image'
+import Image from "next/image";
 
 const Card = (props) => {
   return (
-    <div className="relative w-full h-full   opacity-30 hover:opacity-100 hover:brightness-100  hover:grayscale-0 transition-all ease-in delay-50 duration-200 hover:scale-[1.025] hover:shadow-xl">
+    <div className="  w-full h-full opacity-30 hover:opacity-100 hover:brightness-100  hover:grayscale-0 transition-all ease-in delay-50 duration-200 hover:scale-[1.025] hover:shadow-xl">
       <Image
-          src={props.path}
-          alt={props.title}
-          width={props.width}
-          height={props.height}
-          className="object-cover w-full h-full transition-all ease-in delay-100 duration-200"/>
-
+        src={props.path}
+        alt={props.title}
+        width={props.width}
+        height={props.height}
+        layout="responsive"
+        className="object-cover transition-all ease-in delay-100 duration-200"
+      />
     </div>
   );
 };
--- pages/gallery.js
+++ pages/gallery.js
@@ -6,64 +6,65 @@ export default function Gallery() {
   return (
     <PageLayout>
       <div className=" md:mx-36 md:my-5 grid grid-cols-1 gap-y-6 md:grid-cols-3 md:grid-rows-4 md:gap-6">
-        <div className="md:row-span-2 md:col-span-1 ">
+        <a className="md:row-span-2 md:col-span-1 " id="modal-test" href="/contents/gallery/1.png" target="_blank">
           <Card
             title="Influence"
             subtitle="Design and Development"
-            path="/contents/images/module_animation.png"
+            path="/contents/gallery/1.png"
             width="300"
             height="600"
           />
-        </div>
-        <div className="bg-gray-500 md:row-span-1 md:col-span-2">
-          <Link href="/project">
+        </a>
+
+        <a className=" md:row-span-1 md:col-span-2" href="/contents/gallery/2.png" target="_blank" >
+          <Link href="/contents/gallery/2.png" target>
             <a>
               <Card
                 title="Insomnia"
                 subtitle="Design and Development"
-                path="/contents/images/module_animation.png"
+                path="/contents/gallery/2.png"
                 width="600"
                 height="300"
               />
             </a>
           </Link>
-        </div>
-        <div className=" bg-gray-500 md:row-span-1 md:col-span-1">
+        </a>
+        <a className="md:row-span-1 md:col-span-1" href="/contents/gallery/3.png" target="_blank">
           <Card
             title="Influence"
             subtitle="Design and Development"
-            path="/contents/images/module_animation.png"
+            path="/contents/gallery/3.png"
             width="300"
             height="300"
           />
-        </div>
-        <div className="bg-gray-500 md:row-span-2 md:col-span-1">
+        </a>
+        <a className="md:row-span-2 md:col-span-1" href="/contents/gallery/4.png" target="_blank">
           <Card
             title="Influence"
             subtitle="Design and Development"
-            path="/contents/images/module_animation.png"
+            path="/contents/gallery/4.png"
             width="300"
             height="600"
           />
-        </div>
-        <div className="bg-gray-500 md:row-span-2 md:col-span-2">
+        </a>
+        <a className="md:row-span-2 md:col-span-2" href="/contents/gallery/5.png" target="_blank">
           <Card
             title="Influence"
             subtitle="Design and Development"
-            path="/contents/images/module_animation.png"
+            path="/contents/gallery/5.png"
             width="600"
             height="600"
           />
-        </div>
-        <div className=" bg-gray-500 md:row-span-1 md:col-span-1">
+        </a>
+        <a className="md:row-span-1 md:col-span-1" href="/contents/gallery/6.png" target="_blank">
           <Card
             title="Influence"
             subtitle="Design and Development"
-            path="/contents/images/module_animation.png"
+            path="/contents/gallery/6.png"
             width="300"
             height="300"
           />
-        </div>
+        </a>
       </div>
     </PageLayout>
   );
