8e78789887477f6b4bebe0ca37449649acbc0769
Omicrxn <alexavila0128@gmail.com>
2022-06-13 02:25:01 +0200

    hero layout 2 col

--- layouts/hero.js
+++ layouts/hero.js
@@ -5,9 +5,9 @@ import Spline from "@splinetool/react-spline";
 import React from "react";
 const Hero = () => {
   return (
-    <div className="w-full items-center justify-center grid grid-cols-1 gap-y-6 lg:grid-cols-2 lg:gap-y-10 ">
+    <div className="w-full items-center justify-center grid grid-cols-1 lg:grid-cols-2 gap-y-6 lg:gap-y-0">
       <motion.div
-        className=" text-center lg:text-left "
+        className=" text-center lg:text-left space-y-4"
         initial="hidden"
         animate="visible"
         variants={{
@@ -24,15 +24,21 @@ const Hero = () => {
           },
         }}
       >
-        <h1>Your dream</h1>
-        <h1>our reality</h1>
-        <p className="pt-1">
-          With Phage design and technology become one to bring your dreams to
-          life.
+        <h1 className="text-6xl font-semibold ">Dune: Special Spice Ops</h1>
+        <p className="pt-1 text-xl">
+          A world succumbed by the tirany of the Harkonnen desperate for someone
+          who will deliver the Fremmen. Will you be the one to save them?
         </p>
+        <a
+          download="file"
+          // href={require("")}
+          className="border-none outline-none inline-block py-[10px] px-[25px] bg-dune-orangespicy text-center cursor-pointer no-underline rounded-md text-dune-white font-bold text-lg"
+        >
+          Download Now
+        </a>
       </motion.div>
       <motion.div
-      className="w-full"
+        className="h-full"
         initial="hidden"
         animate="visible"
         variants={{
@@ -49,7 +55,7 @@ const Hero = () => {
           },
         }}
       >
-          <Spline scene="https://prod.spline.design/BaYZhcMXvOtKxU8q/scene.splinecode" />
+        <Spline scene="https://prod.spline.design/BaYZhcMXvOtKxU8q/scene.splinecode" className="h-[60vh]"/>
       </motion.div>
     </div>
   );
